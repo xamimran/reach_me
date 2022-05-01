25780d489a56852ed7cbaf8c8fee5749331793fa
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-05-01 03:46:48 +0200

    'VF'

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if STATUS == 'development':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3217123@localhost/dejahoe'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gumpgtbywlrqoj:8ffcfcd41a565a5241c6f33f1237df2db8e8c25f4895546ad6e800d6b0c84eef@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dbltmodhv650fd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gumpgtbywlrqoj:8ffcfcd41a565a5241c6f33f1237df2db8e8c25f4895546ad6e800d6b0c84eef@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dbltmodhv650fd'
 
 # msearch configuration
 # https://github.com/honmaple/flask-msearch
